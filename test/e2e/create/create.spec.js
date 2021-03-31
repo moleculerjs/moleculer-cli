6e6f9f711dbafd59c2fac7bbd35780ec779e527d
devalexandre <alexandre@indev.net.br>
2021-03-31 19:21:51 -0300

    feature: check cntent in file

--- test/e2e/create/create.spec.js
+++ test/e2e/create/create.spec.js
@@ -6,13 +6,14 @@ const fs = require("fs");
 const create = require("../../../src/create");
 const answers = require("./create_answers.json");
 const answers_ts = require("./create_answers_ts.json");
-
+const file_mock = fs.realpathSync(path.join(__dirname));
 describe("test create",()=>{
 
 	it("create js service",async  (done) => {
 
 		const file = fs.realpathSync(path.join(__dirname,"..","..",".."));
 		const _path = `${file}/${answers.serviceFolder}/${answers.serviceName}.service.js`;
+		const _mock_file = `${file_mock}/${answers.serviceName}.service.js`
 
 		jest.mock("inquirer");
 		inquirer.prompt = jest.fn().mockResolvedValue(answers);
@@ -24,6 +25,9 @@ describe("test create",()=>{
 			.argv;
 		const parser = new YargsPromise(yargs);
 		await parser.parse(`create ${answers.serviceName}`);
+
+		expect(fs.readFileSync(_path)).toEqual(fs.readFileSync(_mock_file))
+
 		expect(fs.existsSync(_path)).toBeTruthy();
 		fs.unlinkSync(_path);
 		jest.clearAllMocks();
@@ -35,6 +39,7 @@ describe("test create",()=>{
 
 		const file = fs.realpathSync(path.join(__dirname,"..","..",".."));
 		const _path = `${file}/${answers_ts.serviceFolder}/${answers_ts.serviceName}.service.ts`;
+		const _mock_file = `${file_mock}/${answers.serviceName}.service.ts`
 
 		jest.mock("inquirer");
 
@@ -48,6 +53,8 @@ describe("test create",()=>{
 
 		const parser = new YargsPromise(yargs);
 		await parser.parse(`create service ${answers_ts.serviceName} --typescript `);
+		expect(fs.readFileSync(_path)).toEqual(fs.readFileSync(_mock_file))
+
 		expect(fs.existsSync(_path)).toBeTruthy();
 		fs.unlinkSync(_path);
 
