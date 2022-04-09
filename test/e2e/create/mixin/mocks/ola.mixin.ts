2d48b2afa343dc134957b0bc320c6fa25bcf31f4
devalexandre <alexandre@indev.net.br>
2022-04-09 14:07:46 -0300

    feat: add mixin, ts template

--- test/e2e/create/mixin/mocks/ola.mixin.ts
+++ test/e2e/create/mixin/mocks/ola.mixin.ts
@@ -1,103 +1,70 @@
-"use strict";
-
-import { existsSync } from "fs";
-import { sync } from "mkdirp";
 import { Context, Service, ServiceSchema } from "moleculer";
-import DbService from "moleculer-db";
+import { Document, Schema, connect, model } from "mongoose";
 
-export default class OlaConnection
-	implements Partial<ServiceSchema>, ThisType<Service>
-{
-	private cacheCleanEventName: string;
-	private collection: string;
+export default class OlaConnection implements Partial<ServiceSchema>, ThisType<Service>{
+
+    private collection: string;
+    private modelSchema: Schema;
 	private schema: Partial<ServiceSchema> & ThisType<Service>;
 
-	public constructor(public collectionName: string) {
+    public constructor(public collectionName: string, modelSchema: Schema) {
 		this.collection = collectionName;
-		this.cacheCleanEventName = `cache.clean.${this.collection}`;
+        this.modelSchema = modelSchema;
 		this.schema = {
-			mixins: [DbService],
-			events: {
-				/**
-				 * Subscribe to the cache clean event. If it's triggered
-				 * clean the cache entries for this service.
-				 *
-				 */
-				async [this.cacheCleanEventName]() {
-					if (this.broker.cacher) {
-						await this.broker.cacher.clean(`${this.fullName}.*`);
-					}
-				},
-			},
-			methods: {
-				/**
-				 * Send a cache clearing event when an entity changed.
-				 *
-				 * @param {String} type
-				 * @param {any} json
-				 * @param {Context} ctx
-				 */
-				entityChanged: async (
-					type: string,
-					json: any,
-					ctx: Context
-				) => {
-					await ctx.broadcast(this.cacheCleanEventName);
-				},
-			},
-			async started() {
-				// Check the count of items in the DB. If it's empty,
-				// Call the `seedDB` method of the service.
-				if (this.seedDB) {
-					const count = await this.adapter.count();
-					if (count === 0) {
-						this.logger.info(
-							`The '${this.collection}' collection is empty. Seeding the collection...`
-						);
-						await this.seedDB();
-						this.logger.info(
-							"Seeding is done. Number of records:",
-							await this.adapter.count()
-						);
-					}
-				}
-			},
-		};
-	}
+            actions: {
+                create: {
+                    rest: "POST /",
+                    async handler(ctx: Context) {
+                        const { params } = ctx;
+                        this.adapter.create(params, (err: Error, saved: Document) => {
+                            if (err) {this.logger.error(err);};
+                            this.logger.info(saved);
+                        });
+                    },
+                },
+                update: {
+                    rest: "PUT /:id",
+                    async handler(ctx: Context<{id: string}>) {
+                        const { params } = ctx;
+                        this.adapter.findOneAndUpdate(
+                            { _id: params.id },
+                            params,
+                            (err: Error, saved: Document) => {
+                                if (err) {this.logger.error(err);}
+                                this.logger.info(saved);
+                            }
+                        );
+                    },
+                },
+                list: {
+                    rest: "GET /",
+                    async handler(ctx: Context) {
+                        return this.adapter.find({});
+                    },
+                },
 
-	public start() {
-		if (process.env.MONGO_URI) {
-			// Mongo adapter
-			// eslint-disable-next-line @typescript-eslint/no-var-requires
-			const MongoAdapter = require("moleculer-db-adapter-mongo");
-			this.schema.adapter = new MongoAdapter(process.env.MONGO_URI);
-			this.schema.collection = this.collection;
-		} else if (process.env.NODE_ENV === "test") {
-			// NeDB memory adapter for testing
-			// @ts-ignore
-			this.schema.adapter = new DbService.MemoryAdapter();
-		} else {
-			// NeDB file DB adapter
-
-			// Create data folder
-			if (!existsSync("./data")) {
-				sync("./data");
-			}
-			// @ts-ignore
-			this.schema.adapter = new DbService.MemoryAdapter({
-				filename: `./data/${this.collection}.db`,
-			});
-		}
-
-		return this.schema;
-	}
-
-	public get _collection(): string {
-		return this.collection;
-	}
-
-	public set _collection(value: string) {
-		this.collection = value;
-	}
-}
-
+                delete: {
+                    rest: "DELETE /:id",
+                    async handler(ctx: Context<{id: string}>) {
+                        const { params } = ctx;
+                        this.adapter.deleteOne({ _id: params.id });
+                    },
+                },
+            },
+            methods: {
+                connectDb() {
+                    return connect("mongodb://localhost:27017/test")
+                        .then(() => this.logger.info("Connected"))
+                        .catch((err: Error) => this.logger.error(err));
+                },
+            },
+            async started(){
+                this.connectDb();
+            },
+        };
+    }
+    public start(){
+        this.schema.adapter = model(this.collection, this.modelSchema);
+        return this.schema;
+    }
+};
